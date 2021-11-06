be9a18417a6aa68c4f61776455704817014857a6
Jose Antonio <joshanccom@gmail.com>
2021-11-06 06:30:37 -0500

    enumeraint for include qr code in my book

--- 1.Blink/1.Blink.ino
+++ 1.Blink/1.Blink.ino
@@ -1,5 +1,5 @@
 //Don't forget to configurate your port, and you can change delay(1000), 1000=1s
-#define LED_BUILTIN 2
+#define LED_BUILTIN 1
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
